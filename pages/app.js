439fa561fdf31a24debe54705aaa5f966cf1cc50
Emanuele Romagnoli <emanuele.romagnoli@robin.it>
2022-09-26 14:19:48 +0200

    setup app views

---
+++ pages/app.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { constants } from "../constants/constants";
+import { AppShell } from "../shared/AppShell";
+import {Box} from "../shared/Typography";
+import {AppList, AppListItem, AppListItemIcon, AppListItemText} from "../shared/AppList";
+import Link from 'next/link';
+import Image from 'next/image';
+import { API } from '../shared/utils';
+// import { useRouter } from 'next/router';
+
+export async function getServerSideProps(context) {
+  const { ...params } = context.query;
+  const query = Object.keys(params).map((key, i) => key + '=' + params[key]).join('&');
+  const endpoint = 'user/?limit=100&page=0&' + query;
+  try {
+      const response = await API(endpoint);
+      return {
+          props: {
+              data: response.data
+          },
+      };
+  } catch (error) {
+      return {
+          props: {
+              error: error
+          },
+      };
+  }
+}
+
+export default function Home({ data }) {
+  return (
+    <AppShell>
+      <div>
+        <Box>
+            <header>
+                <div className="alert-title">
+                    <h3>AVVOCATI</h3>
+                </div>
+            </header>
+            <main>
+              <AppList>
+                  {Object.keys(data).map((key, i) => {
+                      const user = data[key];
+                      return (<Link key={i} href={'/app/user/' + user.id}>
+                        <a>
+                          <AppListItem border>
+                              <AppListItemIcon weight="large">
+                                  <Image alt={user.name} src={constants.remoteBaseUrl + user.picture} width='65' height='65' />
+                              </AppListItemIcon>
+                              <AppListItemText
+                                  title={user.name}
+                                  subtitle={user.places}
+                              />
+                          </AppListItem>
+                        </a>
+                      </Link>)
+                  })}
+              </AppList>
+            </main>
+        </Box>
+    </div>
+    </AppShell>
+  )
+}
---
+++ pages/app/user/[id].js
@@ -0,0 +1,91 @@
+import { Box } from "../../../shared/Typography";
+import { AppList, AppListItem, AppListItemIcon, AppListItemText } from "../../../shared/AppList";
+import Image from "next/image";
+import { API } from "../../../shared/utils";
+import { constants } from "../../../constants/constants";
+import { AppShell } from "../../../shared/AppShell";
+
+export async function getServerSideProps(context) {
+    const { id } = context.query;
+    try {
+        const response = await API('user/' + id);
+        console.log(typeof response)
+        return {
+            props: {
+                data: response.data
+            },
+        };
+    } catch (error) {
+        return {
+            props: {
+                error: error
+            },
+        };
+    }
+}
+
+export default function UserDetailView({ data }) {
+
+    return <AppShell>
+        {/*<Helmet>
+            <title>{`Avv. ${data.name}`}</title>
+            <meta name="description" content={data.summary || "Nessuna descrizione presente"} />
+</Helmet>*/}
+        <Box>
+            <header>
+                <div className="alert-title">
+                    <h3>{data.name}</h3>
+                </div>
+            </header>
+            <main>
+                <AppList>
+                    <AppListItem>
+                        <AppListItemIcon weight="large">
+                            <Image alt={data.name} src={constants.remoteBaseUrl + data.picture} width='65' height='65' />
+                        </AppListItemIcon>
+                        <AppListItemText
+                            title={"Avvocato del Foro di " + data.albo}
+                            subtitle={data.places}
+                        />
+                    </AppListItem>
+                    <AppListItem>
+                        <AppListItemText
+                            title="Tribunali"
+                            subtitle={data.jobs && data.jobs.map((job, i) => {
+
+                                return job.tribunale + ((i + 1) !== data.jobs.length ? ', ' : '')
+                            })}
+                        />
+                    </AppListItem>
+                    <AppListItem>
+                        <AppListItemText
+                            title="Materie"
+                            subtitle={data.areas && data.areas.map((area, i) => {
+
+                                return area.area + ((i + 1) !== data.areas.length ? ', ' : '')
+                            })}
+                        />
+                    </AppListItem>
+                    <AppListItem>
+                        <AppListItemText
+                            title="Presentazione"
+                            subtitle={data.summary ? data.summary : "Nessuna descrizione presente"}
+                        />
+                    </AppListItem>
+                    {data.cv && <AppListItem>
+                        <AppListItemText
+                            title="Curriculum"
+                            subtitle={<a href={data.cv} download={true}>download</a>}
+                        />
+                    </AppListItem>}
+                    <AppListItem>
+                        <AppListItemText
+                            title="Contatti"
+                            subtitle={data.email + (data.phone && data.show_phone ? ', ' + data.phone : '')}
+                        />
+                    </AppListItem>
+                </AppList>
+            </main>
+        </Box>
+    </AppShell>
+}
