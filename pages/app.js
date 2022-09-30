9f9dfb61e70d73f7984e95d5d0cda32450a0d0b1
Emanuele Romagnoli <emanuele.romagnoli@robin.it>
2022-09-30 17:26:49 +0200

    fixed homepage grid

--- pages/app.js
+++ pages/app.js
@@ -6,7 +6,16 @@ import {AppList, AppListItem, AppListItemIcon, AppListItemText} from "../shared/
 import Link from 'next/link';
 import Image from 'next/image';
 import { API } from '../shared/utils';
+import { AppAside } from '../shared/AppAside';
 // import { useRouter } from 'next/router';
+
+const termsRoutes = [];
+const courtList = constants.courts.map(court => {
+    return {
+        title: court,
+        url: "/app/?dati_utente__citta=" + court
+    }
+})
 
 export async function getServerSideProps(context) {
   const { ...params } = context.query;
@@ -14,9 +23,11 @@ export async function getServerSideProps(context) {
   const endpoint = 'user/?limit=100&page=0&' + query;
   try {
       const response = await API(endpoint);
+      const blogResponse = await API('blog/?limit=5&order=creation_date');
       return {
           props: {
-              data: response.data
+              data: response.data,
+              articles: blogResponse.data || constants.test_blogs
           },
       };
   } catch (error) {
@@ -28,10 +39,10 @@ export async function getServerSideProps(context) {
   }
 }
 
-export default function Home({ data }) {
+export default function Home({ data, articles }) {
   return (
     <AppShell>
-      <div>
+        <AppAside title='città' items={courtList} />
         <Box>
             <header>
                 <div className="alert-title">
@@ -39,28 +50,28 @@ export default function Home({ data }) {
                 </div>
             </header>
             <main>
-              <AppList>
-                  {Object.keys(data).map((key, i) => {
-                      const user = data[key];
-                      return (<Link key={i} href={'/app/user/' + user.id}>
+                <AppList>
+                    {Object.keys(data).map((key, i) => {
+                        const user = data[key];
+                        return (<Link key={i} href={'/app/user/' + user.id}>
                         <a>
-                          <AppListItem border>
-                              <AppListItemIcon weight="large">
-                                  <Image alt={user.name} src={constants.remoteBaseUrl + user.picture} width='65' height='65' />
-                              </AppListItemIcon>
-                              <AppListItemText
-                                  title={user.name}
-                                  //subtitle={user.places}
-                                  subtitle={user.areas.map(e => e.area).join(', ')}
-                              />
-                          </AppListItem>
+                            <AppListItem border>
+                                <AppListItemIcon weight="large">
+                                    <Image alt={user.name} src={constants.remoteBaseUrl + user.picture} width='65' height='65' />
+                                </AppListItemIcon>
+                                <AppListItemText
+                                    title={user.name}
+                                    //subtitle={user.places}
+                                    subtitle={user.areas.map(e => e.area).join(', ')}
+                                />
+                            </AppListItem>
                         </a>
-                      </Link>)
-                  })}
-              </AppList>
+                        </Link>)
+                    })}
+                </AppList>
             </main>
         </Box>
-    </div>
+        <AppAside title='blog' items={articles} />
     </AppShell>
   )
 }
